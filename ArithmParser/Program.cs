3f8545b8eccfa69adb4ef7f28c5128c73c804190
djBrinee <deivyjunior12@gmail.com>
2022-11-12 17:37:01 -0400

    Add project files.

---
+++ ArithmParser/Program.cs
@@ -0,0 +1,21 @@
+﻿using Antlr4.Runtime;
+using ArithmParser;
+
+
+string input = "5*6+9*3-1";
+
+Console.WriteLine($"INPUT: {input}\n");
+
+ICharStream stream = CharStreams.fromString(input);
+
+aritmeticaLexer lexer = new aritmeticaLexer(stream);
+
+CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+aritmeticaParser parser = new aritmeticaParser(tokens);
+
+var tree = parser.aritmetica();
+
+Aritmetica aritmetica = new Aritmetica();
+
+int resultado = aritmetica.Visit(tree);
